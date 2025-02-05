f4b7ea17e4b087189efb343e2c61cbb244b166cb
VASKO\eqinox <eqinox@abv.bg>
2025-02-05 12:07:42 +0200

    constants change

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,3 +1,3 @@
-export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Vasko Portfolio";
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Vasko's Website";
 export const APP_DESCRIPTION =
-  process.env.NEXT_PUBLIC_APP_DESCRIPTION || "A modern portfolio";
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION || "A modern website";
